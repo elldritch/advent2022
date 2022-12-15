5612bd2037ebf42e06041b670bf6bf734868e507
Leo Zhang <leo@leozhang.me>
2022-12-15 00:37:04 -0800

    15-1.1: slow, naive implementation

--- src/puzzles/day15.rs
+++ src/puzzles/day15.rs
@@ -1,13 +1,81 @@
-pub fn part1(input: String) -> u32 {
+use std::collections::HashSet;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::{i32, newline},
+    combinator::map,
+    multi::many1,
+    sequence::{terminated, tuple},
+    IResult,
+};
+
+pub fn part1(input: String) -> usize {
     part1_solve(2_000_000, input.as_str())
 }
 
-fn part1_solve(target_row: u32, input: &str) -> u32 {
-    todo!()
+fn part1_solve(target_row: i32, input: &str) -> usize {
+    let sensors = super::shared::must_parse(parse, input);
+
+    // For each sensor, compute its beaconless range, then filter by the target
+    // row.
+    sensors
+        .into_iter()
+        .flat_map(
+            |Sensor {
+                 position,
+                 closest_beacon,
+             }| {
+                let distance = manhattan(position, closest_beacon);
+                (position.0 - distance..=position.0 + distance)
+                    .flat_map(move |x| {
+                        (position.1 - distance..=position.1 + distance).map(move |y| (x, y))
+                    })
+                    .filter(move |candidate| {
+                        manhattan(position, *candidate) <= distance && *candidate != closest_beacon
+                    })
+            },
+        )
+        .filter(|(_, y)| *y == target_row)
+        .collect::<HashSet<_>>()
+        .len()
 }
 
 pub fn part2(_input: String) -> u32 {
     todo!()
+}
+
+fn manhattan(a: Position, b: Position) -> i32 {
+    (a.0 - b.0).abs() + (a.1 - b.1).abs()
+}
+
+type Position = (i32, i32);
+
+#[derive(Debug)]
+struct Sensor {
+    position: Position,
+    closest_beacon: Position,
+}
+
+fn parse(input: &str) -> IResult<&str, Vec<Sensor>> {
+    many1(terminated(
+        map(
+            tuple((
+                tag("Sensor at x="),
+                i32,
+                tag(", y="),
+                i32,
+                tag(": closest beacon is at x="),
+                i32,
+                tag(", y="),
+                i32,
+            )),
+            |(_, sensor_x, _, sensor_y, _, beacon_x, _, beacon_y)| Sensor {
+                position: (sensor_x, sensor_y),
+                closest_beacon: (beacon_x, beacon_y),
+            },
+        ),
+        newline,
+    ))(input)
 }
 
 #[cfg(test)]
